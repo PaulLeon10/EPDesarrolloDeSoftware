c5702734bd9d919fb16e1a3e24824d5437dd4b93
PaulLeon10 <paul.leon.g@uni.pe>
2026-05-15 11:11:08 -0500

    se agrega dto

---
+++ dto/RespuestaUsuario.py
@@ -0,0 +1,9 @@
+class RespuestaUsuario:
+    
+    def __init__(self,id,nombre,tipo,correo):
+        self.id = id
+        self.nombre = nombre
+        self.tipo = tipo
+        self.correo = correo
+        
+        
