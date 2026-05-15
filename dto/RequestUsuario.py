15a6168078d0ef2cefa176a82ba2166eee06d237
PaulLeon10 <paul.leon.g@uni.pe>
2026-05-15 10:29:33 -0500

    Subiendo dto

---
+++ dto/RequestUsuario.py
@@ -0,0 +1,9 @@
+class RespuestaUsuario:
+    
+    def __init__(self,nombre,clave,tipo,correo):
+        self.nombre = nombre
+        self.clave = clave
+        self.tipo = tipo
+        self.correo = correo
+        
+        
